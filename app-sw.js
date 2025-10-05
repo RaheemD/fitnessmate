2e13395cf271f64dfee7a9ebe6549ae8116b047a
RaheemD <165933165+RaheemD@users.noreply.github.com>
2025-10-06 01:14:45 +0530

    Update app-sw.js

--- app-sw.js
+++ app-sw.js
@@ -15,6 +15,12 @@ self.addEventListener('install', (event) => {
   );
 });
 
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
 self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((keys) => Promise.all(keys.map((k) => (k !== CACHE_NAME ? caches.delete(k) : null))))
